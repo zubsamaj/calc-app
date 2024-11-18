6a32380d79a213e2e114b9e6d2cc2d9661b70120
zubsamaj <zubsamaj@gmail.com>
2024-11-18 18:05:09 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+def index():
+   result = ''
+   if request.method == "POST":
+      try:
+         expression = request.form["display"]
+         # Evaluate the expression safely using eval
+         result = eval(expression)
+      except Exception as e:
+         result = "Error"
+   return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0')
