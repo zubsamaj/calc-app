94fb168bb8465aa6155453bf45c3f54227329338
zubsamaj <zubsamaj@gmail.com>
2024-11-18 18:37:32 +0000

    auth token added

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 test_var = 123456
 
+auth_token = 'asvdjasbdvjhgty21312378sgdajsd'
+
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
